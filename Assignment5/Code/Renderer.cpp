8ce238b90c6bf5075eb72f1d9a89f43b8ea239c2
xiangming.lei <xiangming.lei@zqgame.com>
2023-12-18 10:57:49 +0800

    作业5

--- Assignment5/Code/Renderer.cpp
+++ Assignment5/Code/Renderer.cpp
@@ -5,11 +5,12 @@
 #include <optional>
 
 inline float deg2rad(const float &deg)
-{ return deg * M_PI/180.0; }
-
+{
+    return deg * M_PI / 180.0;
+}
 
 // Compute reflection direction
-//计算反射方向 I:入射方向 N：法线方向
+// 计算反射方向 I:入射方向 N：法线方向
 Vector3f reflect(const Vector3f &I, const Vector3f &N)
 {
     return I - 2 * dotProduct(I, N) * N;
@@ -33,7 +34,15 @@ Vector3f refract(const Vector3f &I, const Vector3f &N, const float &ior)
     float cosi = clamp(-1, 1, dotProduct(I, N));
     float etai = 1, etat = ior;
     Vector3f n = N;
-    if (cosi < 0) { cosi = -cosi; } else { std::swap(etai, etat); n= -N; }
+    if (cosi < 0)
+    {
+        cosi = -cosi;
+    }
+    else
+    {
+        std::swap(etai, etat);
+        n = -N;
+    }
     float eta = etai / etat;
     float k = 1 - eta * eta * (1 - cosi * cosi);
     return k < 0 ? 0 : eta * I + (eta * cosi - sqrtf(k)) * n;
@@ -53,14 +62,19 @@ float fresnel(const Vector3f &I, const Vector3f &N, const float &ior)
 {
     float cosi = clamp(-1, 1, dotProduct(I, N));
     float etai = 1, etat = ior;
-    if (cosi > 0) {  std::swap(etai, etat); }
+    if (cosi > 0)
+    {
+        std::swap(etai, etat);
+    }
     // Compute sini using Snell's law
     float sint = etai / etat * sqrtf(std::max(0.f, 1 - cosi * cosi));
     // Total internal reflection
-    if (sint >= 1) {
+    if (sint >= 1)
+    {
         return 1;
     }
-    else {
+    else
+    {
         float cost = sqrtf(std::max(0.f, 1 - sint * sint));
         cosi = fabsf(cosi);
         float Rs = ((etat * cosi) - (etai * cost)) / ((etat * cosi) + (etai * cost));
@@ -71,27 +85,21 @@ float fresnel(const Vector3f &I, const Vector3f &N, const float &ior)
     // kt = 1 - kr;
 }
 
-
-
-
 // [comment]
 // Returns true if the ray intersects an object, false otherwise.
 // （返回光线与物体的交点(距离光源最近的交点)）
 
-// \param orig is the ray origin 
-//（orig:光线的发射位置）
+// \param orig is the ray origin
+// （orig:光线的发射位置）
 
-
-// \param dir is the ray direction 
-//（dir:光线方向）
-
+// \param dir is the ray direction
+// （dir:光线方向）
 
 // \param objects is the list of objects the scene contains
 // （objects:场景中的所有物体）
 
 // \param[out] tNear contains the distance to the cloesest intersected object.
-//（tNear:光线与相交的物体的最近的距离）
-
+// （tNear:光线与相交的物体的最近的距离）
 
 // \param[out] index stores the index of the intersect triangle if the interesected object is a mesh.
 // (index:如果相交的物体是mesh,index存储了相交三角形的索引)
@@ -106,19 +114,19 @@ float fresnel(const Vector3f &I, const Vector3f &N, const float &ior)
 // (isShadowRay:是否是shadowRay)
 // [/comment]
 std::optional<hit_payload> trace(
-        const Vector3f &orig, const Vector3f &dir,
-        const std::vector<std::unique_ptr<Object> > &objects)
+    const Vector3f &orig, const Vector3f &dir,
+    const std::vector<std::unique_ptr<Object>> &objects)
 {
     float tNear = kInfinity;
     std::optional<hit_payload> payload;
 
-    //遍历场景中的所有物体，判断光线与物体是否有交点（没有采用加速结构）
-    for (const auto & object : objects)
+    // 遍历场景中的所有物体，判断光线与物体是否有交点（没有采用加速结构）
+    for (const auto &object : objects)
     {
         float tNearK = kInfinity;
         uint32_t indexK;
         Vector2f uvK;
-        //判断 光线与物体是否有交点
+        // 判断 光线与物体是否有交点
         if (object->intersect(orig, dir, tNearK, indexK, uvK) && tNearK < tNear)
         {
             payload.emplace();
@@ -150,120 +158,115 @@ std::optional<hit_payload> trace(
 // at the intersection point.
 // [/comment]
 Vector3f castRay(
-        const Vector3f &orig, const Vector3f &dir, const Scene& scene,
-        int depth)
+    const Vector3f &orig, const Vector3f &dir, const Scene &scene,
+    int depth)
 {
-    if (depth > scene.maxDepth) {
-        return Vector3f(0.0,0.0,0.0);
+    if (depth > scene.maxDepth)
+    {
+        return Vector3f(0.0, 0.0, 0.0);
     }
 
     Vector3f hitColor = scene.backgroundColor;
-    if (auto payload = trace(orig, dir, scene.get_objects()); payload)//这段代码是使用 C++17 的结构化绑定（Structured Bindings）特性来简化代码
-    {   //光线与场景中的物体有交点
-        
-        //光线与物体的交点
+    if (auto payload = trace(orig, dir, scene.get_objects()); payload) // 这段代码是使用 C++17 的结构化绑定（Structured Bindings）特性来简化代码
+    {                                                                  // 光线与场景中的物体有交点
+
+        // 光线与物体的交点
         Vector3f hitPoint = orig + dir * payload->tNear;
 
-        //hitPoint的法线向量
+        // hitPoint的法线向量
         Vector3f N; // normal
 
-        //hitPoint的纹理坐标
+        // hitPoint的纹理坐标
         Vector2f st; // st coordinates
 
-        //插值运算： 赋值给N和st
+        // 插值运算： 赋值给N和st
         payload->hit_obj->getSurfaceProperties(hitPoint, dir, payload->index, payload->uv, N, st);
 
-        switch (payload->hit_obj->materialType) {
-            case REFLECTION_AND_REFRACTION://物体材质：能反射、能折射
-            {  
-                //反射方向
-                Vector3f reflectionDirection = normalize(reflect(dir, N));
+        switch (payload->hit_obj->materialType)
+        {
+        case REFLECTION_AND_REFRACTION: // 物体材质：能反射、能折射
+        {
+            // 反射方向
+            Vector3f reflectionDirection = normalize(reflect(dir, N));
 
-                //折射方向
-                Vector3f refractionDirection = normalize(refract(dir, N, payload->hit_obj->ior));
+            // 折射方向
+            Vector3f refractionDirection = normalize(refract(dir, N, payload->hit_obj->ior));
 
+            // 反射光线原点
+            Vector3f reflectionRayOrig = (dotProduct(reflectionDirection, N) < 0) ? hitPoint - N * scene.epsilon : hitPoint + N * scene.epsilon;
 
-                //反射光线原点
-                Vector3f reflectionRayOrig = (dotProduct(reflectionDirection, N) < 0) ?
-                                             hitPoint - N * scene.epsilon :
-                                             hitPoint + N * scene.epsilon;
+            // 折射光线原点
+            Vector3f refractionRayOrig = (dotProduct(refractionDirection, N) < 0) ? hitPoint - N * scene.epsilon : hitPoint + N * scene.epsilon;
 
-                //折射光线原点                             
-                Vector3f refractionRayOrig = (dotProduct(refractionDirection, N) < 0) ?
-                                             hitPoint - N * scene.epsilon :
-                                             hitPoint + N * scene.epsilon;
+            // 递归调用得到反射的颜色
+            Vector3f reflectionColor = castRay(reflectionRayOrig, reflectionDirection, scene, depth + 1);
 
-                //递归调用得到反射的颜色
-                Vector3f reflectionColor = castRay(reflectionRayOrig, reflectionDirection, scene, depth + 1);
+            // 递归调用得到折射的颜色
+            Vector3f refractionColor = castRay(refractionRayOrig, refractionDirection, scene, depth + 1);
 
-                //递归调用得到折射的颜色
-                Vector3f refractionColor = castRay(refractionRayOrig, refractionDirection, scene, depth + 1);
+            // 计算发射率
+            float kr = fresnel(dir, N, payload->hit_obj->ior);
 
-                //计算发射率
-                float kr = fresnel(dir, N, payload->hit_obj->ior);
+            // 反射颜色和折射颜色加权
+            hitColor = reflectionColor * kr + refractionColor * (1 - kr);
+            break;
+        }
+        case REFLECTION: // 物体材质：只能反射
+        {
+            // 计算反射率
+            float kr = fresnel(dir, N, payload->hit_obj->ior);
 
-                //反射颜色和折射颜色加权
-                hitColor = reflectionColor * kr + refractionColor * (1 - kr);
-                break;
-            }
-            case REFLECTION://物体材质：只能反射
+            // 反射方向
+            Vector3f reflectionDirection = reflect(dir, N);
+
+            // 反射光线原点
+            Vector3f reflectionRayOrig = (dotProduct(reflectionDirection, N) < 0) ? hitPoint + N * scene.epsilon : hitPoint - N * scene.epsilon;
+            // 递归调用得到反射颜色
+            hitColor = castRay(reflectionRayOrig, reflectionDirection, scene, depth + 1) * kr;
+            break;
+        }
+        default: // 物体材质：不反射也不折射
+        {
+            // [comment]
+            // We use the Phong illumation model int the default case. The phong model
+            // is composed of a diffuse and a specular reflection component.
+            // [/comment]
+            Vector3f lightAmt = 0, specularColor = 0;
+
+            // 阴影点
+            Vector3f shadowPointOrig = (dotProduct(dir, N) < 0) ? hitPoint + N * scene.epsilon : hitPoint - N * scene.epsilon;
+            // [comment]
+            // Loop over all lights in the scene and sum their contribution up
+            // We also apply the lambert cosine law
+            // [/comment]
+            for (auto &light : scene.get_lights())
             {
-                //计算反射率
-                float kr = fresnel(dir, N, payload->hit_obj->ior);
+                // 光线方向
+                Vector3f lightDir = light->position - hitPoint;
 
-                //反射方向
-                Vector3f reflectionDirection = reflect(dir, N);
-                
-                //反射光线原点
-                Vector3f reflectionRayOrig = (dotProduct(reflectionDirection, N) < 0) ?
-                                             hitPoint + N * scene.epsilon :
-                                             hitPoint - N * scene.epsilon;
-                //递归调用得到反射颜色
-                hitColor = castRay(reflectionRayOrig, reflectionDirection, scene, depth + 1) * kr;
-                break;
+                // square of the distance between hitPoint and the light
+                float lightDistance2 = dotProduct(lightDir, lightDir);
+                lightDir = normalize(lightDir);
+
+                float LdotN = std::max(0.f, dotProduct(lightDir, N));
+                // is the point in shadow, and is the nearest occluding object closer to the object than the light itself?
+                auto shadow_res = trace(shadowPointOrig, lightDir, scene.get_objects());
+
+                // 是否在阴影中
+                bool inShadow = shadow_res && (shadow_res->tNear * shadow_res->tNear < lightDistance2);
+
+                lightAmt += inShadow ? 0 : light->intensity * LdotN;
+                Vector3f reflectionDirection = reflect(-lightDir, N);
+
+                // 高光颜色
+                specularColor += powf(std::max(0.f, -dotProduct(reflectionDirection, dir)),
+                                      payload->hit_obj->specularExponent) *
+                                 light->intensity;
             }
-            default://物体材质：不反射也不折射
-            {
-                // [comment]
-                // We use the Phong illumation model int the default case. The phong model
-                // is composed of a diffuse and a specular reflection component.
-                // [/comment]
-                Vector3f lightAmt = 0, specularColor = 0;
 
-                //阴影点
-                Vector3f shadowPointOrig = (dotProduct(dir, N) < 0) ?
-                                           hitPoint + N * scene.epsilon :
-                                           hitPoint - N * scene.epsilon;
-                // [comment]
-                // Loop over all lights in the scene and sum their contribution up
-                // We also apply the lambert cosine law
-                // [/comment]
-                for (auto& light : scene.get_lights()) {
-                    //光线方向
-                    Vector3f lightDir = light->position - hitPoint;
-
-                    // square of the distance between hitPoint and the light
-                    float lightDistance2 = dotProduct(lightDir, lightDir);
-                    lightDir = normalize(lightDir);
-                    
-                    float LdotN = std::max(0.f, dotProduct(lightDir, N));
-                    // is the point in shadow, and is the nearest occluding object closer to the object than the light itself?
-                    auto shadow_res = trace(shadowPointOrig, lightDir, scene.get_objects());
-
-                    //是否在阴影中
-                    bool inShadow = shadow_res && (shadow_res->tNear * shadow_res->tNear < lightDistance2);
-
-                    lightAmt += inShadow ? 0 : light->intensity * LdotN;
-                    Vector3f reflectionDirection = reflect(-lightDir, N);
-
-                    //高光颜色
-                    specularColor += powf(std::max(0.f, -dotProduct(reflectionDirection, dir)),
-                        payload->hit_obj->specularExponent) * light->intensity;
-                }
-
-                hitColor = lightAmt * payload->hit_obj->evalDiffuseColor(st) * payload->hit_obj->Kd + specularColor * payload->hit_obj->Ks;
-                break;
-            }
+            hitColor = lightAmt * payload->hit_obj->evalDiffuseColor(st) * payload->hit_obj->Kd + specularColor * payload->hit_obj->Ks;
+            break;
+        }
         }
     }
 
@@ -275,7 +278,7 @@ Vector3f castRay(
 // primary rays and cast these rays into the scene. The content of the framebuffer is
 // saved to a file.
 // [/comment]
-void Renderer::Render(const Scene& scene)
+void Renderer::Render(const Scene &scene)
 {
     std::vector<Vector3f> framebuffer(scene.width * scene.height);
 
@@ -295,7 +298,24 @@ void Renderer::Render(const Scene& scene)
             // TODO: Find the x and y positions of the current pixel to get the direction
             // vector that passes through it.
             // Also, don't forget to multiply both of them with the variable *scale*, and
-            // x (horizontal) variable with the *imageAspectRatio*            
+            // x (horizontal) variable with the *imageAspectRatio*
+
+            // x:(0,scene.width)  =>  x：(0,1)
+            x = (i + 0.5) / scene.width;
+            // y:(0,scene.height)  =>  x：(0,1)
+            y = (j + 0.5) / scene.height;
+
+            // x:(0,1)   =>  x:(-1,1)
+            x = 2 * x - 1;
+            // y:(0,1)   =>  y:(1,-1)
+            y = -2 * y + 1;
+
+            // 对于宽高比！=1的屏幕，像素不是正方形，x坐标乘上宽高比
+            x = x * imageAspectRatio;
+
+            // scale = tan(fov/2) ,摄像机到近裁剪平面的距离 = 1 ，所以 宽高需要乘缩放比例 scale
+            x = x * scale;
+            y = y * scale;
 
             Vector3f dir = Vector3f(x, y, -1); // Don't forget to normalize this direction!
             framebuffer[m++] = castRay(eye_pos, dir, scene, 0);
@@ -304,14 +324,15 @@ void Renderer::Render(const Scene& scene)
     }
 
     // save framebuffer to file
-    FILE* fp = fopen("binary.ppm", "wb");
+    FILE *fp = fopen("binary.ppm", "wb");
     (void)fprintf(fp, "P6\n%d %d\n255\n", scene.width, scene.height);
-    for (auto i = 0; i < scene.height * scene.width; ++i) {
+    for (auto i = 0; i < scene.height * scene.width; ++i)
+    {
         static unsigned char color[3];
         color[0] = (char)(255 * clamp(0, 1, framebuffer[i].x));
         color[1] = (char)(255 * clamp(0, 1, framebuffer[i].y));
         color[2] = (char)(255 * clamp(0, 1, framebuffer[i].z));
         fwrite(color, 1, 3, fp);
     }
-    fclose(fp);    
+    fclose(fp);
 }
--- Assignment5/Code/Triangle.hpp
+++ Assignment5/Code/Triangle.hpp
@@ -4,42 +4,41 @@
 
 #include <cstring>
 
-bool rayTriangleIntersect(const Vector3f& v0, const Vector3f& v1, const Vector3f& v2, const Vector3f& orig,
-                          const Vector3f& dir, float& tnear, float& u, float& v)
+bool rayTriangleIntersect(const Vector3f &v0, const Vector3f &v1, const Vector3f &v2, const Vector3f &orig,
+                          const Vector3f &dir, float &tnear, float &u, float &v)
 {
     // TODO: Implement this function that tests whether the triangle
     // that's specified bt v0, v1 and v2 intersects with the ray (whose
     // origin is *orig* and direction is *dir*)
     // Also don't forget to update tnear, u and v.
 
-    auto e1 = v1-v0;
-    auto e2 = v2-v0;
+    auto e1 = v1 - v0;
+    auto e2 = v2 - v0;
     auto s = orig - v0;
-    auto s1 = dir.crossProduct(e2);
-    auto s2 = s.crossProduct(e1);
+    auto s1 = crossProduct(dir, e2);
+    auto s2 = crossProduct(s, e1);
 
-    auto divise = s1.dotProduct(e1)
+    auto divise = dotProduct(s1, e1);
 
-    float t = s2.dotProduct(e2)/divise;
-    float b1 = s1.dotProduct(s)/divise;
-    float b2 = s2.dotProduct(dir)/divise;
+    float t = dotProduct(s2, e2) / divise;
+    float b1 = dotProduct(s1, s) / divise;
+    float b2 = dotProduct(s2, dir) / divise;
 
-    if (t>0.0&&b1>0.0&&b2>0.0&&(1-b1-b2)>0.0)
+    if (t > 0.0 && b1 > 0.0 && b2 > 0.0 && (1 - b1 - b2) > 0.0)
     {
         u = b1;
         v = b2;
         tnear = t;
         return true;
     }
-    
-    
+
     return false;
 }
 
 class MeshTriangle : public Object
 {
 public:
-    MeshTriangle(const Vector3f* verts, const uint32_t* vertsIndex, const uint32_t& numTris, const Vector2f* st)
+    MeshTriangle(const Vector3f *verts, const uint32_t *vertsIndex, const uint32_t &numTris, const Vector2f *st)
     {
         uint32_t maxIndex = 0;
         for (uint32_t i = 0; i < numTris * 3; ++i)
@@ -55,18 +54,17 @@ public:
         memcpy(stCoordinates.get(), st, sizeof(Vector2f) * maxIndex);
     }
 
-
-    //判断是否相交
-    bool intersect(const Vector3f& orig, const Vector3f& dir, float& tnear, uint32_t& index,
-                   Vector2f& uv) const override
+    // 判断是否相交
+    bool intersect(const Vector3f &orig, const Vector3f &dir, float &tnear, uint32_t &index,
+                   Vector2f &uv) const override
     {
         bool intersect = false;
-        //遍历物体的所有三角形，判断与光线是否有交点
+        // 遍历物体的所有三角形，判断与光线是否有交点
         for (uint32_t k = 0; k < numTriangles; ++k)
         {
-            const Vector3f& v0 = vertices[vertexIndex[k * 3]];
-            const Vector3f& v1 = vertices[vertexIndex[k * 3 + 1]];
-            const Vector3f& v2 = vertices[vertexIndex[k * 3 + 2]];
+            const Vector3f &v0 = vertices[vertexIndex[k * 3]];
+            const Vector3f &v1 = vertices[vertexIndex[k * 3 + 1]];
+            const Vector3f &v2 = vertices[vertexIndex[k * 3 + 2]];
             float t, u, v;
             if (rayTriangleIntersect(v0, v1, v2, orig, dir, t, u, v) && t < tnear)
             {
@@ -81,22 +79,22 @@ public:
         return intersect;
     }
 
-    void getSurfaceProperties(const Vector3f&, const Vector3f&, const uint32_t& index, const Vector2f& uv, Vector3f& N,
-                              Vector2f& st) const override
+    void getSurfaceProperties(const Vector3f &, const Vector3f &, const uint32_t &index, const Vector2f &uv, Vector3f &N,
+                              Vector2f &st) const override
     {
-        const Vector3f& v0 = vertices[vertexIndex[index * 3]];
-        const Vector3f& v1 = vertices[vertexIndex[index * 3 + 1]];
-        const Vector3f& v2 = vertices[vertexIndex[index * 3 + 2]];
+        const Vector3f &v0 = vertices[vertexIndex[index * 3]];
+        const Vector3f &v1 = vertices[vertexIndex[index * 3 + 1]];
+        const Vector3f &v2 = vertices[vertexIndex[index * 3 + 2]];
         Vector3f e0 = normalize(v1 - v0);
         Vector3f e1 = normalize(v2 - v1);
         N = normalize(crossProduct(e0, e1));
-        const Vector2f& st0 = stCoordinates[vertexIndex[index * 3]];
-        const Vector2f& st1 = stCoordinates[vertexIndex[index * 3 + 1]];
-        const Vector2f& st2 = stCoordinates[vertexIndex[index * 3 + 2]];
+        const Vector2f &st0 = stCoordinates[vertexIndex[index * 3]];
+        const Vector2f &st1 = stCoordinates[vertexIndex[index * 3 + 1]];
+        const Vector2f &st2 = stCoordinates[vertexIndex[index * 3 + 2]];
         st = st0 * (1 - uv.x - uv.y) + st1 * uv.x + st2 * uv.y;
     }
 
-    Vector3f evalDiffuseColor(const Vector2f& st) const override
+    Vector3f evalDiffuseColor(const Vector2f &st) const override
     {
         float scale = 5;
         float pattern = (fmodf(st.x * scale, 1) > 0.5) ^ (fmodf(st.y * scale, 1) > 0.5);
